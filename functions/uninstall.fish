27675f35c2a4d2b3b0310446f8b993ca1ed77626
Jethro Kuan <jethrokuan95@gmail.com>
2016-02-28 16:16:56 +0800

    Unset variables after uninstall

--- functions/uninstall.fish
+++ functions/uninstall.fish
@@ -1,2 +1,10 @@
 functions -e __z_on_variable_pwd
 functions -e $Z_CMD
+
+printf "$Z_DATA will be left intact...\n"
+printf "to remove, simply delete it\n"
+
+#Unset variables
+set -e Z_CMD
+set -e Z_DATA
+set -e Z_EXCLUDE
