6b5a6f0fc64f8e94a86eb8d179f97289ea0e0cbd
Francisco Lourenço <aristidesfl@users.noreply.github.com>
2017-05-07 20:29:22 +0200

    Minor code style change

--- functions/__z_complete.fish
+++ functions/__z_complete.fish
@@ -1,5 +1,5 @@
 function __z_complete -d "add completions"
-  set -l __z_marks (string replace -r '\|.*' '' <$Z_DATA | string escape)
+  set -l __z_marks (string replace -r '\|.*' '' < $Z_DATA | string escape)
 
   complete -c $Z_CMD -a "$__z_marks" -f
   complete -c $ZO_CMD -a "$__z_marks" -f
