eea1bdb22f189017c416c631866ac9055a4fbeb0
Francisco Lourenço <aristidesfl@users.noreply.github.com>
2017-05-07 20:27:41 +0200

    Add support for paths with single/double quotes

--- functions/__z_complete.fish
+++ functions/__z_complete.fish
@@ -1,8 +1,5 @@
 function __z_complete -d "add completions"
-  set -l __z_marks (awk '
-    BEGIN { FS="|"}
-    /1/ {print $1}
-    ' < $Z_DATA)
+  set -l __z_marks (string replace -r '\|.*' '' <$Z_DATA | string escape)
 
   complete -c $Z_CMD -a "$__z_marks" -f
   complete -c $ZO_CMD -a "$__z_marks" -f
