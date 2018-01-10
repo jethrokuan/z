0aeb3876a3c79357f57ae60fc32076ffd72d456b
Jethro Kuan <jethrokuan95@gmail.com>
2018-01-11 02:44:57 +0800

    Update awk file to match rupa's z (#44)

--- functions/__z.fish
+++ functions/__z.fish
@@ -2,6 +2,7 @@ function __z -d "Jump to a recent directory."
   set -l option
   set -l arg
   set -l typ ''
+  set -l list
   set -g z_path (command dirname (status -f))
   set -l target
 
@@ -20,7 +21,7 @@ function __z -d "Jump to a recent directory."
         set arg "$2"
         break
       case l list
-        set option "list"
+        set list "list"
         set arg "$2"
         break
       case r rank
@@ -44,37 +45,37 @@ function __z -d "Jump to a recent directory."
         printf "         -t --recent   Search by recency, cd\n"
         printf "         -h --help     Print this help\n\n"
         printf "If installed with fisherman, run `fisher help z` for more info"
-          return 0
+        return 0
       case \*
         printf "$Z_CMD: '%s' is not a valid option\n" $1
         __z --help
         return 1
-        end
-    end
-
-    if test 1 -eq (printf "%s" $arg | grep -c "^\/")
-      set target $arg
-    else
-      set target (command awk -v t=(date +%s) -v option="$option" -v typ="$typ" -v q="$arg" -F "|" -f $z_path/z.awk "$Z_DATA")
-    end
-
-    if test "$option" = "list"
-      echo "$target" | tr ";" "\n" | sort -nr
-      return 0
-    end
-
-    if test "$status" -gt 0
-      return
-    end
-
-    if test -z "$target"
-      printf "'%s' did not match any results" "$arg"
-      return 1
-    end
-
-    if contains -- ech $option
-      printf "%s\n" "$target"
-    else if not contains -- list $option
-      pushd "$target"
     end
   end
+
+  if test 1 -eq (printf "%s" $arg | grep -c "^\/")
+    set target $arg
+  else
+    set target (command awk -v t=(date +%s) -v list="$list" -v typ="$typ" -v q="$arg" -F "|" -f $z_path/z.awk "$Z_DATA")
+  end
+
+  if test "$list" = "list"
+    echo "$target" | tr ";" "\n" | sort -nr
+    return 0
+  end
+
+  if test "$status" -gt 0
+    return
+  end
+
+  if test -z "$target"
+    printf "'%s' did not match any results" "$arg"
+    return 1
+  end
+
+  if contains -- ech $option
+    printf "%s\n" "$target"
+  else if not contains -- list $option
+    pushd "$target"
+  end
+end
