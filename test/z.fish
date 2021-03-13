02f9cd98d7833ebddecc251e31bb2468febcbcf3
Johannes Altmanninger <aclopte@gmail.com>
2021-03-13 08:46:13 +0100

    Fix tests
    I'd prefer to switch to littlecheck but this works

--- test/z.fish
+++ test/z.fish
@@ -1,101 +1,67 @@
-set pth $DIRNAME/$TESTNAME
+set -xg pth (mktemp -d)
 
-function setup
-  set -U Z_DATA "$pth/.z"
-  mkdir -p $pth/{foo,bar}
-  # we can't have | because it is the separator in $Z_DATA
-  set -xg special '(){}#$%^<>?*"\'\\ &	'
-  mkdir -p $pth/{foo,bar,$special}
-  touch $Z_DATA
+set -xg Z_DATA "$pth/.z"
+mkdir -p $pth/{foo,bar}
+# we can't have | because it is the separator in $Z_DATA
+set -xg special '(){}#$%^<>?*"\'\\ &	'
+mkdir -p $pth/{foo,bar,$special}
+touch $Z_DATA
 
-  for i in foo bar $special
-    cd $pth/$i
-  end
+function cd_some
+    z --clean
+    for i in foo bar $special
+        cd $pth/$i
+    end
 end
 
-function teardown
-  rm -rf $pth
-end
+cd_some
 
-test ".z is created"
-  -f $Z_DATA
-end
+echo pth $pth >&2
+echo Z_DATA $Z_DATA >&2
+@test ".z is created" -f $Z_DATA
 
-test "Z_CMD is set"
-  ! -z $Z_CMD
-end
+@test "Z_CMD is set" ! -z $Z_CMD
 
-test "has foo"
-  0 -eq (grep -q foo $Z_DATA; echo $status)
-end
+@test "has foo" 0 -eq (grep -q foo $Z_DATA; echo $status)
 
-test "has bar"
-  0 -eq (grep -q bar $Z_DATA; echo $status)
-end
+@test "has bar" 0 -eq (grep -q bar $Z_DATA; echo $status)
 
-test "has special"
-  0 -eq (fish -c 'grep -qF $special $Z_DATA'; echo $status)
-end
+@test "has special" 0 -eq (fish -c 'grep -qF $special $Z_DATA'; echo $status)
 
-test "! has kid"
-  1 -eq (grep -q kid $Z_DATA; echo $status)
-end
+@test "! has kid" 1 -eq (grep -q kid $Z_DATA; echo $status)
 
-test "z --purge"
-  -z (z --purge > /dev/null; cat $Z_DATA)
-end
+@test "z --purge" -z (z --purge > /dev/null; cat $Z_DATA)
 
-test "z --clean"
-  1 -eq (echo '$pth/invalid_path|1|1501234567' >> $Z_DATA;
-         z --clean > /dev/null;
-         grep -q invalid_path $Z_DATA;
-         echo $status)
-end
+@test "z --clean" 1 -eq (
+        echo '$pth/invalid_path|1|1501234567' >> $Z_DATA;
+        z --clean > /dev/null;
+        grep -q invalid_path $Z_DATA;
+        echo $status
+     )
+cd_some
 
-test "z -e foo"
-  $pth/foo = (z -e foo)
-end
+@test "z -e foo" $pth/foo = (z -e foo)
 
-test "! z -e kid"
-  (printf "'kid' did not match any results\n1") = (z -e kid; echo $status)
-end
+@test "! z -e kid" 1 = (z -e kid >/dev/null; echo $status)
 
-test "z -h"
-  0 -eq (z -h | grep -q Usage; echo $status)
-end
+@test "z -h" 0 -eq (z -h | grep -q Usage; echo $status)
 
-test "z foo"
-  $pth/foo = (z foo; and echo $PWD)
-end
+@test "z foo" $pth/foo = (z foo; and echo $PWD)
 
-test "z bar"
-  $pth/bar = (z bar; and echo $PWD)
-end
+@test "z bar" $pth/bar = (z bar; and echo $PWD)
 
-test "f oo"
-  $pth/foo = (z f oo; and echo $PWD)
-end
+@test "f oo" $pth/foo = (z f oo; and echo $PWD)
 
-test "fo oo"
-  $pth/foo != (z fo oo; and echo $PWD)
-end
+@test "fo oo" $pth/foo != (z fo oo; and echo $PWD)
 
-test "z kid"
-  (printf "'kid' did not match any results\n1") = (z kid; and echo $PWD $status; or echo $status)
-end
+@test "z kid" 1 = (z kid >/dev/null; echo $status)
 
-test "z special"
-  0 -eq (fish -c 'z $special' >/dev/null; echo $status)
-end
+@test "z special" 0 -eq (fish -c 'z $special' >/dev/null; echo $status)
 
-test "z --list foo"
-  $pth/foo = (z --list foo ^/dev/null | awk '{ print $2} ')
-end
+@test "z --list foo" $pth/foo = (z --list foo 2>/dev/null | awk '{ print $2} ')
 
-test "list common path on stderr"
-  "common:    $pth/foo" = (z --list foo 2>&1 >/dev/null | grep common:)
-end
+@test "list common path on stderr" "common:    $pth/foo" = (z --list foo 2>&1 >/dev/null | grep common:)
 
-test "z -x works"
-  (printf "'foo' did not match any results\n1") = (z foo; and z -x; and cd ..; and z foo; and echo $PWD $status; or echo $status;)
-end
+@test "z -x works" 1 = (begin; z foo; and z -x; and cd ..; and z foo; end >/dev/null; echo $status)
+
+rm -rf $pth
