52d1b509f38f1b9ac6b55c9b192659834d3b3c4c
Mike Lei <mikelei8291@users.noreply.github.com>
2021-03-23 00:53:21 +0000

    Add argument completion for "-x/--delete"

--- functions/__z_complete.fish
+++ functions/__z_complete.fish
@@ -13,4 +13,5 @@ function __z_complete -d "add completions"
   complete -c $Z_CMD -s r -l rank   -d "Searches by rank, cd"
   complete -c $Z_CMD -s t -l recent -d "Searches by recency, cd"
   complete -c $Z_CMD -s h -l help   -d "Print help"
+  complete -c $Z_CMD -s x -l delete -d "Removes the current directory from $Z_DATA"
 end
